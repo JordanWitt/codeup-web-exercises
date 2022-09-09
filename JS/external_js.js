81da7db6b8e33fd21c360610e9d682766b307581
Jordan Witt <wittjordan94@gmail.com>
2022-09-09 14:38:21 -0500

    pushing practice material

--- JS/external_js.js
+++ JS/external_js.js
@@ -1,21 +1,44 @@
 "use strict";
-console.log("sanity check - is this hooked up");
-let gmMessage ="Good morning, how confused ae you?";
-
-alert("User Detected");
-
-alert(gmMessage);
-
-//alert SHOW MY INFO make a BUTTON no USER INTERACTION
-//asking user true or false question
-
-// let hadBreakfast =confirm("did you eat this morning");
+// console.log("sanity check - is this hooked up");
+// let welcomeMSG ="Welcome to my Website";
 //
-// alert("Hey, you told me that is was" + hadBreakfast "is that going to be tue");
+// alert(welcomeMSG);
 //
-// let whatInTheWorld = prompt(message:"What in the world is going on");
+// let favoriteColor= prompt("What is your favorite color?");
 //
-// alert("I don't think you're right" + whatInTheWorld + "look somewhere else, away you fool");
+// alert("is a nasty color " + favoriteColor);
+//
+// let TLM = 3;
+// let BB =  5;
+// let herc = 1;
+// let price = (TLM + BB + herc) * 3;
+// console.log(price);
+// let moviesGalore = "Welcome to Movies";
+//     alert(moviesGalore);
+// // alerts the user the website name
+// let Hercules = prompt("How many days would you like to rent Hercules?");
+// let movieDays = "Awesome now get out";
+//     alert(movieDays);
+// // a prompt for the Hercules movie
+// let theLittleMermaid = prompt("How many days would you like to rent The Little Mermaid?");
+//     alert(movieDays);
+// // prompt for The Little Mermaid
+// let brotherBear = prompt("How many days would you like to rent Brother Bear?");
+//     alert(movieDays);
+// // prompt for the brother bear
+// let totalPrice = prompt("Your total is 27");
+// alert(movieDays);
+
+// Question 2
+
+// let spaceInClass = confirm("Is there space in the class?");
+// let spaceInSchedule = confirm("Does the time support your needs");
+// let canBeEnrolled = spaceInClass && spaceInSchedule
+// alert("Can you enroll?" + canBeEnrolled);
+
+// Question 3
+
+
 
 
 
---
+++ dtpractice.js
@@ -0,0 +1,65 @@
+"use strict";
+//Question 3a
+//
+let tlm = 3;
+let bb = 5;
+let herc = 1;
+let price = (tlm + bb + herc) * price;
+
+console.log(price);
+
+//Question 3b
+
+let googlePay = 400;
+let amazonPay = 380;
+let facebookPay = 350;
+
+let gTotal = googlePay * 6;
+let aTotal = amazonPay * 4;
+let fTotal = facebookPay * 10;
+
+let paycheck = gTotal + aTotal + fTotal;
+console.log(paycheck);
+
+//Question 3c
+
+//canBeEnrolled = IF NOT FULL and SCHEDULE DOES NOT CONFLICT
+
+let spaceInClass = true;
+
+let spaceInSchedule = true;
+
+let canBeEnrolled = spaceInClass && spaceInSchedule
+
+console.log("Can I be enrolled?:" + canBeEnrolled);
+
+//Question 3d
+
+//offerCanBeApplied = IF MORE THAN 2 ITEMS and OFFER NOT EXPIRED or IS PREMIUM MEMBER
+
+let offerNotExpired = true;
+let moreThan2 = true;
+let isPremiumMember = false;
+
+let offerCanBeApplied = offerNotExpired && (moreThan2 || isPremiumMember);
+
+console.log("it is true that" + offerNotExpired + "and that you have" + moreThan2
+"more than" + "one item" + "but I see" + isPremiumMember "you are not a premium member");
+
+//Question 3e
+
+let username = "codeup";
+let password = "notastrongpassword"
+
+// the password must be at least 5 characters
+
+let passwordOverFive = password.legnth >=5;
+let noUserNameInPassword = password == (password.indexOf(username) == -1;
+// the username must be no more than 20 characters
+let userNameLengthOkay = username.length < 20;
+let noWhiteSpace =!((username.startsWith("") || username.endsWith("")) || (password.startsWith("") || password.endsWith("")));
+
+
+if (noUserNameInPassword && passwordOverFive && userNameLengthOkay && noWhiteSpace) {
+    console.log("You Username and Password are good!")
+}
