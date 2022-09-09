0fa15a5cde4b54cde926abcda2ef308fc261d60e
Jordan Witt <wittjordan94@gmail.com>
2022-09-09 12:20:21 -0500

    this is an updated external for the JS excersie

---
+++ JS/external_js.js
@@ -0,0 +1,21 @@
+"use strict";
+console.log("sanity check - is this hooked up");
+let gmMessage ="Good morning, how confused ae you?";
+
+alert("User Detected");
+
+alert(gmMessage);
+
+//alert SHOW MY INFO make a BUTTON no USER INTERACTION
+//asking user true or false question
+
+// let hadBreakfast =confirm("did you eat this morning");
+//
+// alert("Hey, you told me that is was" + hadBreakfast "is that going to be tue");
+//
+// let whatInTheWorld = prompt(message:"What in the world is going on");
+//
+// alert("I don't think you're right" + whatInTheWorld + "look somewhere else, away you fool");
+
+
+
