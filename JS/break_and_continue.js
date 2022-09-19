2b9e656e98daf8c22ea67e53ddfb0d491db87951
Jordan Witt <wittjordan94@gmail.com>
2022-09-19 15:13:37 -0500

    finished arrays exercise on planets_js

--- JS/break_and_continue.js
+++ JS/break_and_continue.js
@@ -1,17 +1,17 @@
 // "use strict";
 //console.log("sanity check");
-let userInput = "";
-while(true){
-    userInput = prompt("enter in odd number between 1-50");
-    if(userInput % 2 !== 0 && userInput <= 50 && userInput >= 1){
-        break;
-    }
-        //console.log(userInput)
-}
-for(let i = 1; i < 50; i+=2){
-    if(userInput == i) {
-        console.log("skipping " + userInput)
-        continue;
-    }console.log("your odd number " + i);
-}
-//console.log(userInput);
+// let userInput = "";
+// while(true){
+//     userInput = prompt("enter in odd number between 1-50");
+//     if(userInput % 2 !== 0 && userInput <= 50 && userInput >= 1){
+//         break;
+//     }
+//         //console.log(userInput)
+// }
+// for(let i = 1; i < 50; i+=2){
+//     if(userInput == i) {
+//         console.log("skipping " + userInput)
+//         continue;
+//     }console.log("your odd number " + i);
+// }
+// //console.log(userInput);
---
+++ JS/while.js
@@ -0,0 +1,20 @@
+"use strict";
+// let number = 2
+// while(number <= 65536){
+//     console.log(number);
+//     number = number * 2;
+// }
+//Question Do While
+let allCones = Math.floor(Math.random() * 50) + 50;
+
+if{// want x amount of cones
+    else // only have x amount left
+        console.log
+    else if {
+        console.log//
+    }
+}
+
+
+
+// Math.floor(Math.random() * 5) + 1;
