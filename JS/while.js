645709a640f8047129126bc96c2270603768ae31
Jordan Witt <wittjordan94@gmail.com>
2022-09-23 15:34:54 -0500

    completed ice cream exercise in the while.js

--- JS/while.js
+++ JS/while.js
@@ -6,7 +6,21 @@
 // }
 //Question Do While
 let allCones = Math.floor(Math.random() * 50) + 50;
-do
-    let canBuy = Math.floor(Math.random() * 5) + 1
-    if (canBuy < 0) ;
-    console.log("Cannot sell you 6 cones I only have 3" + allCones)
+do {
+    let conesSold = Math.floor(Math.random() * 5) + 1
+
+    console.log("You would like " + conesSold + " I have " + allCones + " left to sell.");
+
+    if(conesSold > allCones){
+        console.log("There's not enough cones, scat.");
+        continue;
+    }
+    allCones -= conesSold;
+    console.log(" I just sold " + conesSold + " now I have " + allCones + " left to sell.");
+}while(allCones > 0)
+
+
+    //     let conesSold = Math.floor(Math.random() * 5) + 1
+//     if (canBuy < 0) ;
+//     console.log("Cannot sell you 6 cones I only have 3" + allCones)
+// }
