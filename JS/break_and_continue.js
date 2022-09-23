eca5f2d17c04590474697bfee3d806fae9854e96
Jordan Witt <wittjordan94@gmail.com>
2022-09-23 15:05:54 -0500

    pushing welcome w/out images, need to get the images sourced

--- JS/break_and_continue.js
+++ JS/break_and_continue.js
@@ -1,17 +1,18 @@
-// "use strict";
-//console.log("sanity check");
-// let userInput = "";
-// while(true){
-//     userInput = prompt("enter in odd number between 1-50");
-//     if(userInput % 2 !== 0 && userInput <= 50 && userInput >= 1){
-//         break;
-//     }
-//         //console.log(userInput)
-// }
-// for(let i = 1; i < 50; i+=2){
-//     if(userInput == i) {
-//         console.log("skipping " + userInput)
-//         continue;
-//     }console.log("your odd number " + i);
-// }
-// //console.log(userInput);
+"use strict";
+console.log("sanity check");
+let userInput = "";
+while(true){
+    userInput = prompt("enter in odd number between 1-50");
+    if(userInput % 2 !== 0 && userInput <= 50 && userInput >= 1){
+        break;
+    }
+        //console.log(userInput)
+}
+for(let i = 1; i < 50; i+=2){
+    if(userInput == i) {
+        console.log("skipping " + userInput)
+        continue;
+    }console.log("your odd number " + i);
+}
+//console.log(userInput);
+// pushing break and continue
