804e1f60d633a9f177c56b79a622ad558e0954b2
Jordan Witt <wittjordan94@gmail.com>
2022-10-19 14:28:38 -0500

    finished the selectors exercise for the jquery lesson

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,3 +1,13 @@
-$(function(){
-    alert('The DOM has finished loading!');
-})
+// $(function(){
+//     alert('The DOM has finished loading!');
+// })
+$('.code-up').css('border', '1px solid #F00');
+//adds border around the class of code-up
+$('li').css('font-size', '20px');
+
+$('h1, p, li').css('background-color', '#FF0');
+//highlighting the contents yellow that are in the elements h1, p, and li
+
+let alertH1 = $('h1').html();
+alert(alertH1);
+//alerting anthing thats in the contents of the h1 element
