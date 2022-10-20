2b2419f64e5026c3cbe15f3de36bd16e2dcb74fe
Jordan Witt <wittjordan94@gmail.com>
2022-10-20 10:10:51 -0500

    trying functions for konami code

---
+++ js/jquery.js
@@ -0,0 +1,10 @@
+// "use strict";
+// console.log("hello world");
+// //testing link
+//
+//
+// //logging the key events when pressed "released" in the console.
+// const secretCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13];
+// const keyPress = [];
+
+
