8a2f8e6fb7acf2f4c9a4610fce2aac93fc977192
Jordan Witt <wittjordan94@gmail.com>
2022-10-26 14:12:31 -0500

    added interval to display date

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,1 +1,30 @@
 "use strict";
+const timeEl = document.getElementById('time');
+const dateEl = document.getElementById('date');
+const currentWeatherItemsEl = document.getElementById('current-weather-items');
+const timezone = document.getElementById('timezone');
+const country = document.getElementById('country');
+const weatherForecastEl = document.getElementById('weather-forecast');
+const currentTempEl = document.getElementById('current-temp');
+
+
+console.log(openweatherKey); //seeing if the key is alive
+
+const days =['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const months =['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+//setting the current date and time on the page
+setInterval(() => {
+    const time = new Date();
+    const month = time.getMonth();
+    const date = time.getDate();
+    const day = time.getDay();
+    const hour = time.getHours();
+    const hoursIn12rFormat = hour >= 13 ? hour %12: hour
+    const minutes = time.getMinutes();
+    const ampm = hour >=12 ? 'PM' : 'AM'
+
+    timeEl.innerHTML = hoursIn12rFormat + ':' + minutes + '' +  `<span id="am-pm">${ampm}</span>`
+    dateEl.innerHTML = days[day] + ', ' + date + ' ' + months[month]
+
+}, 1000);
+
