791cd622ceebdc21c05dcb8f0a7d6edd092fb901
Jordan Witt <wittjordan94@gmail.com>
2022-10-26 13:14:08 -0500

    adding to my weather maps

--- js/keys.js
+++ js/keys.js
@@ -1,4 +1,4 @@
 "use strict";
 
-const OPEN_WEATHER_API = 0e6535f71fcf176bb9afd2f35f2a2c92
-const MAPBOX_KEY = pk.eyJ1Ijoid2l0dC1qb3JkYW4iLCJhIjoiY2w5bjg2NmYzMDMxMzNubXoya3NkdmFmNCJ9.XeAO7FLOUKOLrnRxYiQvgw
+const openweatherKey = '0e6535f71fcf176bb9afd2f35f2a2c92';
+const mapboxKey = 'pk.eyJ1Ijoid2l0dC1qb3JkYW4iLCJhIjoiY2w5bjg2NmYzMDMxMzNubXoya3NkdmFmNCJ9.XeAO7FLOUKOLrnRxYiQvgw';
